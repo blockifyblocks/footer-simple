a6d59b24972886c7ca3c78727555c4e1641d13d7
Steve62 <steve@62design.co.uk>
2014-07-23 11:03:19 +0100

    Added a simple navigation

--- footer-simple.php
+++ footer-simple.php
@@ -1,7 +1,19 @@
-<?php
-
-$block->open('footer');
+<?php $block->open('footer');
 
 $block->document->tag('span', 'name');
-
-$block->close();
+?>
+    <nav itemscope itemtype="http://schema.org/SiteNavigationElement">
+        <ul>
+            <?php
+            $block->document->each('@list', function ($prop, $item) use ($block) {
+                $item->open('li');
+                $isButton = $item['@type'] === 'Thing/Button';
+                $class = $isButton ? 'btn-default' : 'btn-link';
+                ?>
+                <a class="btn <?= $class; ?>" href="<?= $item['url']; ?>" itemprop="url"><?= $item['name']; ?></a>
+                <?php
+                $item->close();
+            }); ?>
+        </ul>
+    </nav>
+<?php $block->close(); ?>
